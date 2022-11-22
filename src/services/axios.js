9e1a79facba8ff67ef725833302f636f03cccb39
rui4ymipat <59613118+rui4ymipat@users.noreply.github.com>
2022-11-22 20:01:47 +0700

    tiw susu

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-axios.defaults.baseURL =
-  "http://localhost:5000/apk-scientific/asia-southeast2";
 // axios.defaults.baseURL =
-//   "https://asia-southeast2-apk-scientific.cloudfunctions.net";
+//   "http://localhost:5000/apk-scientific/asia-southeast2";
+axios.defaults.baseURL =
+  "https://asia-southeast2-apk-scientific.cloudfunctions.net";
 axios.defaults.withCredentials = false;
 
 export default axios;
