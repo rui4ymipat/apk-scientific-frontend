eba46a225cb05e0e4ecc1febcce64855ff0467cf
rui4ymipat <59613118+rui4ymipat@users.noreply.github.com>
2022-12-21 00:01:36 +0700

    change new api

--- src/services/product.js
+++ src/services/product.js
@@ -4,14 +4,15 @@ export const getProductbyCategory = async (
   category,
   page,
   limit,
-  suggest = false
+  suggest = false,
+  nextPage
 ) => {
   const response = await axios.get(
     `/product_list?${
       category ? `category=${category}&` : ""
     }pageSize=${limit}&pageNumber=${page}${
       suggest ? `&suggest=${suggest}` : ""
-    }`
+    }${nextPage ? `&nextPage=${nextPage}` : ""}`
   );
   return response.data;
 };
